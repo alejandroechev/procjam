6e51af3b1fa048273ffe7ed1cec449448a106adc
Cristián Donoso C <cristian@magnet.cl>
2014-11-14 23:55:40 -0300

    SetMarked method for tiles

--- js/tile.js
+++ js/tile.js
@@ -11,20 +11,27 @@
    this.tileHeight = tileHeight;
    this.color = color;
    this.colorIndex = colorIndex;
+   this.mark = 0;
   }
 
   Tile.prototype = {
-      draw: function(context) {
-        context.fillStyle = this.color == 'black' ? '#459633' : '#9dd2f2';
-        context.strokeStyle = this.color == 'black' ? '#459633' : '#9dd2f2';
-        context.lineWidth = 1;
-        context.fillRect(this.x, this.y, this.tileWidth, this.tileHeight);
-        context.strokeRect(this.x, this.y, this.tileWidth, this.tileHeight);
-      },
-      clone: function() {
-        return new Tile(this.x, this.y, this.i, this.j,
-          this.color, this.colorIndex);
-      }
+    clone: function() {
+      return new Tile(this.x, this.y, this.i, this.j,
+        this.color, this.colorIndex);
+    },
+    draw: function(context) {
+      context.fillStyle = this.color == 'black' ? '#459633' : '#9dd2f2';
+      context.strokeStyle = this.color == 'black' ? '#459633' : '#9dd2f2';
+      context.lineWidth = 1;
+      context.fillRect(this.x, this.y, this.tileWidth, this.tileHeight);
+      context.strokeRect(this.x, this.y, this.tileWidth, this.tileHeight);
+    },
+    setMarked: function() {
+      if (this.mark === 1) return;
+      this.color = 'black';
+      this.colorIndex = -1;
+      this.mark = 1;
+    }
   };
 
   // We export the tile
